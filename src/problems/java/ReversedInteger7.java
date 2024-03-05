8a2a96ebaf4fabd7b8cdcb13e86907fb5c7baa4d
MykolaMuts <muts.mykola@gmail.com>
2024-03-05 18:59:29 +0100

    7. Reversed Integer

--- src/problems/java/ReversedInteger7.java
+++ src/problems/java/ReversedInteger7.java
@@ -6,29 +6,67 @@ import java.util.Collections;
 
 public class ReversedInteger7 {
 
-    static int reverse = 0;
-
-//    public static int reverse(int x) {
-//        if (x == 0) {
-//            return x;
-//        } else {
-//            reverse = 10 * reverse + x % 10;
-//            reverse(x/10);
-//            return reverse;
-//        }
-//    }
-
     public int reverse(int x) {
 
+        double res = reverseHelper(x,0);
+
+        if(res>(double)(Integer.MAX_VALUE) || res<(double)(Integer.MIN_VALUE)){
+            return 0;
+        }
+        return (int) res;
+    }
+
+    /**
+     * That's a recursion solution that use helper function
+     * @param x number that need to be reverse
+     * @return reversed int
+     */
+
+    public double reverseHelper(int x, double reversed) {
+        if (x == 0) {
+            return reversed;
+
+        } else {
+            return reverseHelper(x / 10, 10 * reversed + x % 10);
+        }
+    }
+
+    static int reverse = 0;
+
+    /**
+     /**
+     * That's a recursion solution that use static value
+     * @param x number that need to be reverse
+     * @return reversed int
+     */
+
+    public int reverseStatic(int x) {
+        if (x == 0) {
+            return x;
+        } else {
+            reverse = 10 * reverse + x % 10;
+            reverseStatic(x/10);
+            return reverse;
+        }
+    }
+
+
+    /**
+     * Solution that use array of int that will be reversed and convert back to int
+     * @param x number that need to be reverse
+     * @return reversed int
+     */
+    public int reverseArray(int x) {
+
         int belowZero = 1;
-        
+
         if(x <= 0){
             belowZero = -1;
             x *= belowZero;
         }
-        
+
         int length = (int) Math.log10(x) + 1;
-        
+
         int[] array = new int[length];
 
         int rev = 0;
@@ -37,12 +75,16 @@ public class ReversedInteger7 {
             rev += (rev + (x % 10)) * 10;
             x /= 10;
         }
-        
+
         return rev * belowZero;
 
     }
 
-
+    /**
+     * Solution that use log to reverse int
+     * @param x number that need to be reverse
+     * @return reversed int
+     */
     public static int reverseLog(int x) {
         int reverse = 0;
         int belowZero = 1;
@@ -60,36 +102,29 @@ public class ReversedInteger7 {
         }
     }
 
-
-    public static int pow(int value, int powValue) {
-        if (powValue == 1) {
-            return value;
-        } else {
-            return value * pow(value, powValue - 1);
+    /**
+     * Solution that use loop to reverse int
+     * @param x number that need to be reverse
+     * @return reversed int
+     */
+    public static int reverseLoop(int x) {
+        int degree = (int) Math.log10(x);
+        System.out.println(degree);
+        int y = 0;
+        for (int i = degree; i >= 0; i--) {
+            int pow = (int) Math.pow(10, i);
+            System.out.println("y + " + pow);
+            y += pow;
         }
+        return y;
     }
-
-
-
-//    public static int reverseLoop(int x) {
-//        int degree = (int) Math.log10(x);
-//        System.out.println(degree);
-//        int y = 0;
-//        for (int i = degree; i >= 0; i--) {
-//            int pow = (int) Math.pow(10, i);
-//            System.out.println("y + " + pow);
-//            y += pow;
-//        }
-//        return y;
-//
-//    }
 
     public static void test(){
 
 
         ReversedInteger7 reversedInteger7 = new ReversedInteger7();
 
-        int rev = reversedInteger7.reverse(-132423);
+        int rev = reversedInteger7.reverse(1534236469);
         System.out.println(rev);
     }
 }
